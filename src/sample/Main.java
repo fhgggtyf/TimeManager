2bcf77c3f72c02b51861f38eaf952dcb3731f9cc
Jason Xia <xjc18502795710@outlook.com>
2021-07-14 16:37:31 +0800

    Added initialization of output file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,12 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Calendar;
+
 public class Main extends Application {
 
     @Override
@@ -17,7 +23,24 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        File file = new File("C:\\Users\\夏嘉程\\IdeaProjects\\TimeManager(1)\\out\\data\\out.out");
+        OutputStream output = new FileOutputStream(file);
+        if(!file.getParentFile().exists()){ //如果文件的目录不存在
+            file.getParentFile().mkdirs(); //创建目录
+        }
+
+        Calendar initDay = Calendar.getInstance();
+        initDay.set(2021, 6, 1, 0, 0, 0);
+        for (int i = 0; i < 3652; i++) {
+            initDay.add(Calendar.DATE, 1);
+            String msg = "date: " + initDay + ";\teventList: \n\n";
+            byte data[] = msg.getBytes();
+            output.write(data);
+        }
+        output.close();
+
+
         launch(args);
     }
 }
